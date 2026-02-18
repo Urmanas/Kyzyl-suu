13a5958ae0b35b70fddaff4be063ada98df2c6d6
Urmanas <Urmanas.19@gmail.com>
2026-02-18 09:45:02 +0600

    Make it authenticated

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path 
 
-from .views import home, tour_list, about, contact
+from .views import home, tour_list, about, contact, signup
+
+app_name = 'core'
 
 urlpatterns = [
     path('', home, name='home'),
     path('tours/', tour_list, name='tour_list'),
     path('about/', about, name='about'),
     path('contact/', contact, name='contact'),
+    path('signup/', signup, name='signup'),
 ]
